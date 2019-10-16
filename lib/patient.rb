8ceae2c27aab95cc0394a38943069c718f637138
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-16 15:39:43 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,7 +15,7 @@ class Patient
   
   def new_appointment(doctor, date)
     #Appointment Class args are (date, patient, doctor)
-    Appointment.new(self, date, doctor)
+    Appointment.new(date, self, doctor)
   end
   
   def appointments
