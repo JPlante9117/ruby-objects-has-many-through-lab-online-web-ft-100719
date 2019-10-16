e8ed6224b789008ef76ab74be2762cdced96669d
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-16 14:58:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,6 +5,15 @@ class Doctor
   @@all_docs = []
   def initialize(name)
     @name = name
+    @@all_docs << self
+  end
+  
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+  
+  def new_appointment(date, patient)
+    
   end
   
 end
